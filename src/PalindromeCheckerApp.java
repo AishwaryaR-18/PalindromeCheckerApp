021fc7cc09f2b269f2e7bfb91aa71bedfc6e8674
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 13:46:41 +0530

    feature/UC12

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 
-public class PalindromeCheckerApp {
+interface PalindromeStrategy {
+    boolean isPalindrome(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String input) {
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        PalindromeStrategy strategy;
+
+        strategy = new StackStrategy();
+
+        boolean result = strategy.isPalindrome(input);
+
+        if (result) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+
+        strategy = new DequeStrategy();
+
+        result = strategy.isPalindrome(input);
+
+        if (result) {
+            System.out.println(input + " is a Palindrome (checked using DequeStrategy)");
+        } else {
+            System.out.println(input + " is not a Palindrome (checked using DequeStrategy)");
+        }
+    }
 }
