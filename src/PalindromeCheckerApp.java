3961aa69d28ba6ad1899130a79b5f5f1838696ee
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 13:39:47 +0530

    feature/UC7

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Deque;
+import java.util.LinkedList;
 
-public class PalindromeCheckerApp {
+public class Main {
+
+    public static void main(String[] args) {
+
+        String input = "refer";
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+    }
 }
