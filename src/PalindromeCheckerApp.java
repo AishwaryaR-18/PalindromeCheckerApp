d0d4a0c516ba28e85cb53bfdb0474f4f63b76b0d
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 11:48:16 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
 
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the app!");
+
+        String original = "level";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is not a Palindrome");
+        }
+
+    }
 }
