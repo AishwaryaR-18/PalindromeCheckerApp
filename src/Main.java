dc7e5e94c04685a5f3a0b209572623fae5ed4d76
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 11:35:59 +0530

    UC2

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main{
+
+
+
+    public static void main(String[] args) {
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
