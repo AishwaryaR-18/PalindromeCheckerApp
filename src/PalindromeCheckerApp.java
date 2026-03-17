3c516ab3ec1d0c22de2a71a1fdcd6606652c51a2
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 13:45:21 +0530

    feature/UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class PalindromeChecker {
 
-public class PalindromeCheckerApp {
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if (result) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+    }
 }
