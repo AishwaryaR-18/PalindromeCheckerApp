4b95ce908096a07d20414c11e3f0ae34478c453a
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 13:44:01 +0530

    feature/UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
 
-public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        String input = "A man a plan a canal Panama";
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+    }
 }
