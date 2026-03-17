d3a386cc1f921c1017527fc7d6e4d1c2973d2e9b
AishwaryaR-18 <aishu180806@gmail.com>
2026-03-17 11:54:12 +0530

    feature/UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
 
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        String input = "noon";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+    }
+}
 }
